7b214f1b509e7331d29a5e81d0fcd50cc8747d3f
Lrd134 <lches3200@gmail.com>
2021-07-27 22:43:26 -0500

    commit tree 1e0728ce898dbd9d2cfe451bf49e5b627a26dacc

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,18 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
+    
   end
 
   def show
     @artist = Artist.find(params[:id])
   end
+  def song_index
 
+  end
+  def song
+
+  end
   def new
     @artist = Artist.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :artists
   resources :songs
+  get '/artists/:artist_id/songs/:id', to: 'songs#show', as: 'artist_song'
+  get '/artists/:artist_id/songs', to: 'songs#index', as: 'artist_songs'
 end
